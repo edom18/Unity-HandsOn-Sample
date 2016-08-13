fcc8dcb70dca76dbecc1525d3aaf9448f01cf79c
edo_m18 <edo.m18@gmail.com>
2016-08-13 21:04:17 +0900

    完成版とサンプル版を分ける

--- Assets/Scripts/BulletEx.cs
+++ Assets/Scripts/BulletEx.cs
@@ -5,12 +5,14 @@ public class BulletEx : MonoBehaviour {
 
     public GameObject Detonator;
 
+    // 発射
     public void Shot(Vector3 force)
     {
         var rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(force, ForceMode.Impulse);
     }
 
+    // 他のオブジェクトとぶつかったときの処理
     void OnCollisionEnter(Collision other)
     {
         GameObject detonator = Instantiate(Detonator, transform.position, Quaternion.identity) as GameObject;
