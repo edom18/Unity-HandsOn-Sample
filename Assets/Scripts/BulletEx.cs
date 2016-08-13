36e2bca9496ab3d7c33fddf55b2d430203f1e90a
edo_m18 <edo.m18@gmail.com>
2016-08-13 20:48:20 +0900

    いくつかのアセットを追加

--- Assets/Scripts/BulletEx.cs
+++ Assets/Scripts/BulletEx.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class BulletEx : MonoBehaviour {
 
+    public GameObject Detonator;
+
     public void Shot(Vector3 force)
     {
         var rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(force, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GameObject detonator = Instantiate(Detonator, transform.position, Quaternion.identity) as GameObject;
+        Destroy(gameObject);
     }
 }
